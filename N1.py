febb1522ae8f0603a8f9958f3a8981fbb11255f2
Enzooow <sabanai.enzo@gmail.com>
2021-09-12 17:48:22 -0300

    Create N1.py

---
+++ N1.py
@@ -0,0 +1,19 @@
+print("Você está na sala: 1")
+print("Escolha seu caminho:")
+print("[1] - Caminho vermelho")
+print("[2] - Caminho preto")
+caminho = int(input())
+
+def default():
+  print("Você está na sala:")
+
+interação = 1
+while interação < 7:
+  interação += 1
+else:
+  print("Você e sua guilda se perderam na dungeon e acabaram morrendo pelas criaturas místicas que viviam nela!")
+  print("GGGGG   AAA   M   M  EEEEE      OOO   V   V  EEEEE  RRRR")
+  print("G      A   A  MM MM  E         O   O  V   V  E      R   R")
+  print("G GGG  AAAAA  M M M  EEEEE     O   O   V V   EEEEE  RRRR")
+  print("G   G  A   A  M   M  E         O   O   V V   E      R  R")
+  print("GGGGG  A   A  M   M  EEEEE      OOO     V    EEEEE  R   R")
