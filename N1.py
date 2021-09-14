af357f1fac7b9b6376fc2f3752567649371e1727
Enzooow <sabanai.enzo@gmail.com>
2021-09-14 02:05:49 -0300

    Update N1.py

--- N1.py
+++ N1.py
@@ -1,23 +1,22 @@
-def mensagem_automática():
-  print("Você está na sala: ")
-  print("Escolha seu caminho:")
-  print("[1] - Caminho vermelho")
-  print("[2] - Caminho preto")
-  caminho = int(input())
+def salas(numero_sala):
+    print("Você está na sala: " + numero_sala)
+    print("Escolha seu caminho:")
+    print("[1] - Caminho vermelho")
+    print("[2] - Caminho preto")
+    caminho = int(input())
 
-mensagem_automática()
 
 interação = 1
 
 while interação < 7:
 
-  mensagem_automática()
-  interação +=1
+    salas("1")
+    interação += 1
 
 else:
-  print("Você e sua guilda se perderam na dungeon e acabaram morrendo pelas criaturas místicas que viviam nela!")
-  print("GGGGG   AAA   M   M  EEEEE      OOO   V   V  EEEEE  RRRR")
-  print("G      A   A  MM MM  E         O   O  V   V  E      R   R")
-  print("G GGG  AAAAA  M M M  EEEEE     O   O   V V   EEEEE  RRRR")
-  print("G   G  A   A  M   M  E         O   O   V V   E      R  R")
-  print("GGGGG  A   A  M   M  EEEEE      OOO     V    EEEEE  R   R")
+    print("Você e sua guilda se perderam na dungeon e acabaram morrendo pelas criaturas místicas que viviam nela!")
+    print("GGGGG   AAA   M   M  EEEEE      OOO   V   V  EEEEE  RRRR")
+    print("G      A   A  MM MM  E         O   O  V   V  E      R   R")
+    print("G GGG  AAAAA  M M M  EEEEE     O   O   V V   EEEEE  RRRR")
+    print("G   G  A   A  M   M  E         O   O   V V   E      R  R")
+    print("GGGGG  A   A  M   M  EEEEE      OOO     V    EEEEE  R   R")
